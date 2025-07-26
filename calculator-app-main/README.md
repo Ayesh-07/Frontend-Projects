e5027c8f9e6b28514d7797b505f39aa411a1ae39
Ayesha Nudrat <135106789+Ayesh-07@users.noreply.github.com>
2025-07-26 16:34:18 +0500

    Add files via upload

---
+++ calculator-app-main/README.md
@@ -0,0 +1,34 @@
+
+## Overview
+
+- What is a Calculator? 
+A calculator helps us do math like: 
+● Add (+) 
+● Subtract (-) 
+● Multiply (*) 
+● Divide (/) 
+We want to make this using JavaScript, so users can click buttons (like numbers and 
+operators), and we show the result on the screen.
+
+
+
+
+### Screenshot
+
+![Desktop-Design](./design/desktop-design.png)
+![Mobile-Design](./design/mobile-design.png)
+
+### Links
+
+- Solution URL: [https://github.com/Ayesh-07/Frontend-Projects/tree/main/stats-preview-card]
+- Live Site URL: [https://frontend-projects-k6xe.vercel.app/]
+
+## Process
+- Calculatorʼs Working Like a Human Brain 
+Think of the calculator like this: 
+1. Waiting for first number 
+2. Got first number → waiting for operator 
+3. Got operator → waiting for second number 
+4. Got second number → waiting for = 
+5. Got = → shows result 
+6. User clicks again → repeat from start
---
+++ calculator-app-main/script.js
@@ -0,0 +1,67 @@
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
+let result = '';
+
+let btn = document.querySelectorAll('.calculator__key');
+let screen = document.querySelector('.screen-background');
+
+btn.forEach((ele) => {
+    ele.addEventListener('click', function() {
+        let value = ele.innerText;
+        console.log('Button clicked:', value);
+        screen.setAttribute('style', 'font-size:1.5rem; padding:30px; color:white');
+
+        if (!isNaN(value) || value === '.') {
+
+
+            // Number or dot pressed
+            if (operator === '') {
+                firstNumber += value;
+                screen.innerText = firstNumber;
+            } else {
+                secondNumber += value;
+                screen.innerText = firstNumber + " " + operator +" "+ secondNumber;
+            }
+        } 
+        // Operator is pressed
+        else if (value === '+' || value === '-' || value === '*' || value === '/') {
+       
+            operator = value;
+          screen.innerText = firstNumber + " " + operator ;
+
+            console.log('Operator selected:', operator);
+        } 
+
+     // Equal button is pressed
+        else if (value === '=') {
+
+            
+       
+            let num1 = parseFloat(firstNumber);
+            let num2 = parseFloat(secondNumber);
+
+            if (operator === '+') {
+                result = num1 + num2;
+            } else if (operator === '-') {
+                result = num1 - num2;
+            } else if (operator === '*') {
+                result = num1 * num2;
+            } else if (operator === '/') {
+                result = num1 / num2;
+            }
+
+            screen.innerText = result;
+            
+        } 
+
+        // Reset button is pressed
+        else if (value === 'Reset') {
+           firstNumber = '';
+            secondNumber = '';
+            operator = '';
+            result = '';
+            screen.innerText = '0';
+        }
+    });
+});
