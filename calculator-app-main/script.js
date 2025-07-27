0a07c0a6642acb0ccffd556d182e67c302e6888d
Ayesha Nudrat <135106789+Ayesh-07@users.noreply.github.com>
2025-07-27 22:41:45 +0500

    Update script.js

--- calculator-app-main/script.js
+++ calculator-app-main/script.js
@@ -6,8 +6,11 @@ let result = '';
 let btn = document.querySelectorAll('.calculator__key');
 let screen = document.querySelector('.screen-background');
 
+
+
 btn.forEach((ele) => {
     ele.addEventListener('click', function() {
+
         let value = ele.innerText;
         console.log('Button clicked:', value);
         screen.setAttribute('style', 'font-size:1.5rem; padding:30px; color:white');
@@ -24,53 +27,53 @@ btn.forEach((ele) => {
             
             }else {
                 secondNumber += value;
-                screen.innerText = firstNumber + " " + operator +" "+ secondNumber;
+                screen.innerText = eval(firstNumber + " " + operator +" "+ secondNumber);
             }
              
 
             
         } 
+
+        
         // Operator is pressed
-        else if (value === '+' || value === '-' || value === '*' || value === '/') {
-       
-            operator = value;
-          screen.innerText = firstNumber + " " + operator ;
-
-            console.log('Operator selected:', operator);
-        } 
-
-
-
-     // Equal button is pressed
-        else if (value === '=') {
-            
-            let num1 = parseFloat(firstNumber);
-            let num2 = parseFloat(secondNumber);
-
-            if (operator === '+') {
-                result = num1 + num2;
+        else if ( value === '+' || value === '-' || value === '*' || value === '/') {
+          operator = value;
+          if (operator === '+') {
+                result = firstNumber + secondNumber;
             } else if (operator === '-') {
-                result = num1 - num2;
+                result = firstNumber + secondNumber;
             } else if (operator === '*') {
-                result = num1 * num2;
+                result = firstNumber + secondNumber;
             } else if (operator === '/') {
-                result = num1 / num2;
+                result = firstNumber / secondNumber;
                 if(operator === '/' & secondNumber === '0'){
                  alert("Error : Cannot divide by 0")
                 }
             }
+                screen.innerText = firstNumber + " " + operator ;
+                console.log('Operator selected:', operator);
+            } 
 
-            screen.innerText = result;
-            
-        } 
+    
+        // = is pressed 
 
-        // Reset button is pressed
-        else if (value === 'Reset') {
-           firstNumber = '';
+        else if (value === '=') {
+            firstNumber = '';
             secondNumber = '';
             operator = '';
             result = '';
             screen.innerText = '0';
         }
+        
+        
+
+        else if ('Ans')
+            
+            {
+               screen.innerText = screen.innerText.slice(0 ,-1)
+               
+            }
+
+      
     });
 });
