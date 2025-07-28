5656c93c41af1e07bd9c4377803d94ce03c905cf
Ayesha Nudrat <135106789+Ayesh-07@users.noreply.github.com>
2025-07-28 17:25:56 +0500

    Update script.js

--- calculator-app-main/script.js
+++ calculator-app-main/script.js
@@ -1,33 +1,39 @@
+//  Declare varible for storing the values 
+
 let firstNumber = '';
 let operator = '';
 let secondNumber = '';
 let result = '';
 
+// Select the classes from index.html
+
 let btn = document.querySelectorAll('.calculator__key');
 let screen = document.querySelector('.screen-background');
 
 
+// Apply Event Listener for event handler
 
 btn.forEach((ele) => {
     ele.addEventListener('click', function() {
-
         let value = ele.innerText;
         console.log('Button clicked:', value);
-        screen.setAttribute('style', 'font-size:1.5rem; padding:30px; color:white');
+        screen.setAttribute('style', 'font-size:1.5rem; padding:30px; color:white');  // set the styling for input
 
+        
+        
+        // Number or dot pressed
+        
         if (!isNaN(value) || value === '.') {
 
 
-            // Number or dot pressed
-            if (operator === '') {
+                if (operator === '') {
                 firstNumber += value;
-                screen.innerText = firstNumber;
+                screen.innerText = firstNumber;}
                 
-
-            
-            }else {
+                else {
                 secondNumber += value;
-                screen.innerText = eval(firstNumber + " " + operator +" "+ secondNumber);
+                 screen.innerText = (firstNumber + " " + operator +" "+ secondNumber);
+               
             }
              
 
@@ -36,8 +42,11 @@ btn.forEach((ele) => {
 
         
         // Operator is pressed
+
         else if ( value === '+' || value === '-' || value === '*' || value === '/') {
+
           operator = value;
+
           if (operator === '+') {
                 result = firstNumber + secondNumber;
             } else if (operator === '-') {
@@ -54,10 +63,26 @@ btn.forEach((ele) => {
                 console.log('Operator selected:', operator);
             } 
 
+
+
+        // Condition for double operator
+        
+        else if (operator !== '' && secondNumber === '') {
+        operator = value; 
+        screen.innerText = firstNumber + " " + operator;}
+
     
         // = is pressed 
 
         else if (value === '=') {
+            result = eval(firstNumber + operator + secondNumber);
+             screen.innerText = result;
+
+        }
+
+        // AC is pressed 
+
+        else if (value === 'AC') {
             firstNumber = '';
             secondNumber = '';
             operator = '';
@@ -66,14 +91,20 @@ btn.forEach((ele) => {
         }
         
         
-
-        else if ('Dell')
-            
-            {
-               screen.innerText = screen.innerText.slice(0 ,-1)
-               
+        // Dell is pressed
+        
+         else if (value === 'Dell') {
+            if (secondNumber !== '') {
+                secondNumber = secondNumber.slice(0, -1);
+                screen.innerText = secondNumber || '0';
+            } else if (operator !== '') {
+                operator = '';
+                screen.innerText = firstNumber;
+            } else if (firstNumber !== '') {
+                firstNumber = firstNumber.slice(0, -1);
+                screen.innerText = firstNumber || '0';
             }
-
+        }
       
     });
 });
