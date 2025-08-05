d5e6fa0d0b68e4ea5888e39bee5dc64962b69e63
Ayesha Nudrat <135106789+Ayesh-07@users.noreply.github.com>
2025-08-05 12:13:42 +0500

    Update main.js

--- To-Do-List/main.js
+++ To-Do-List/main.js
@@ -1,127 +1,130 @@
-
 let arr = JSON.parse(localStorage.getItem('Array')) || [];
 
-
 // Add Tasks
+let input = document.getElementById('input');
+let addBtn = document.querySelector('.add');
+let display = document.querySelector('.display');
 
 
-let input = document.getElementById('input')
-let addBtn = document.querySelector('.add')
-let display = document.querySelector('.display')
 
 
+// Declare a global variable to store the value of the task 
+
+let oldTaskValue = null;
+
+function createList(itemText) {
+
+    let container = document.createElement('div');
+    var li = document.createElement('li');
+    let deletebtn = document.createElement('button');
+    let editBtn = document.createElement('button');
+
+    editBtn.textContent = 'Edit';
+    deletebtn.textContent = 'Delete';
+    li.textContent = itemText;
+
+    container.className = 'container';
+    li.style.margin = '16px 0';
+    deletebtn.style.width = '80px';
+    deletebtn.style.backgroundColor = '#D92C54';
+    editBtn.style.width = '80px';
+    editBtn.style.backgroundColor = '#8ABB6C';
+
+    container.appendChild(li);
+    container.appendChild(deletebtn);
+    container.appendChild(editBtn);
+
+    display.appendChild(container);
+
+
+
+    // Delete Task
+    deletebtn.addEventListener('click', () => {
+        let value = li.innerText.trim();
+        let index = arr.indexOf(value);
+
+        if (index !== -1) {
+            arr.splice(index, 1);
+            container.remove();
+            console.log(`${value} is Deleted`);
+            localStorage.setItem('Array', JSON.stringify(arr));
+        } else {
+            alert(`${value} is not Found`);
+        }
+    });
+
+    // Update add Button to Update Task btn
+    
+    editBtn.addEventListener('click', () => {
+        input.value = li.innerText.trim();
+        addBtn.innerText = 'Update Task';
+        addBtn.style.backgroundColor = '#932F67';
+        
+        // Store the old value in the global variable
+        oldTaskValue = li.innerText.trim();
+    });
+}
+
+
+arr.forEach(item => {
+    createList(item);
+});
+
+// Edit Task and update
 addBtn.addEventListener('click', () => {
+    let inputValue = input.value.trim();
 
-    if (input.value !== '') {
-        if (!arr.includes(input.value)) {
+    if (inputValue !== '') {
+        if (addBtn.innerText === 'Update Task') {
+            
+            let index = arr.indexOf(oldTaskValue);
 
-            arr.push(input.value)
-            input.value = ''
+            if (index !== -1) {
+                arr.splice(index, 1, inputValue);
+                localStorage.setItem('Array', JSON.stringify(arr));
+                
+               
+                display.innerHTML = '';
+                arr.forEach(item => {
+                    createList(item);
+                });
 
-            display.innerHTML = '';
-            arr.forEach((item) => {
-                let li = document.createElement('li');
-                li.textContent = item;
-                display.appendChild(li);
-            })
-
-            localStorage.setItem('Array', JSON.stringify(arr))
+                input.value = '';
+                addBtn.innerText = 'Add Task';
+        
+                oldTaskValue = null;
+            }
 
         } else {
-            alert('Duplicate Value is not Allow :)')
+           
+            if (!arr.includes(inputValue)) {
+                arr.push(inputValue);
+                input.value = '';
+
+              
+                createList(inputValue);
+
+                localStorage.setItem('Array', JSON.stringify(arr));
+            } else {
+                alert('Duplicate Value is not Allow :)');
+            }
         }
-
-
-
     } else {
-        alert('Please write something :)')
+        alert('Please write something :)');
     }
-
-
-})
-
-// Delete Task
-
-
-let deletebtn = document.querySelector('.delete')
-let deletedTask = document.querySelector('#del')
-let delTask = document.querySelector('.deleted')
-
-deletebtn.addEventListener('click', () => {
-
-
-    let value = deletedTask.value.trim()
-    let index = arr.indexOf(value)
-
-    if (index !== -1) {
-
-        arr.splice(index, 1)
-        deletedTask.value = ''
-        delTask.innerText = (`${value} is Deleted`)
-
-        display.innerText = arr.join(',')
-        localStorage.setItem('Array', JSON.stringify(arr))
-
-    } else {
-        delTask.innerText = (`${value} is not Found`)
-
-    }
-
-
-
-})
-
-
-//Edit Task
-
-
-let oldTask = document.querySelector('#oldTask')
-let newTask = document.querySelector('#newTask')
-let editBtn = document.querySelector('.edit')
-let editedTask = document.querySelector('.editedTask')
-editBtn.addEventListener('click', () => {
-
-
-    let old = oldTask.value.trim()
-    let newT = newTask.value.trim()
-    let oldValue = arr.indexOf(old)
-
-    if (oldValue !== -1) {
-
-        arr.splice(oldValue, 1, newT)
-        oldTask.value = ''
-        newTask.value = ''
-        display.innerText = arr.join(',')
-        editedTask.innerText = 'Your old value is Updated'
-
-        localStorage.setItem('Array', JSON.stringify(arr))
-
-    } else {
-        editedTask.innerText = 'Your old value is not found'
-
-    }
-
-})
+});
 
 
 
 
-
-// All Clear Tasks
-
-
-let clearbtn = document.querySelector('.clear')
-let removeAll = document.querySelector('.remove')
+// Delete All Tasks
+let clearbtn = document.querySelector('.clear');
 
 clearbtn.addEventListener('click', () => {
-    arr.length = 0
-    console.log(arr)
-    display.innerText = arr.join(',')
-
-    removeAll.innerText = 'All the Tasks are deleted :('
-
-    localStorage.setItem('Array', JSON.stringify(arr))
-})
-
-
-
+    let con = confirm('Are You Want to Delete All Task ?');
+    if (con === true) {
+        arr.length = 0;
+        display.innerHTML = ''; 
+        localStorage.setItem('Array', JSON.stringify(arr));
+    }
+});
